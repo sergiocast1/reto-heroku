c9f44860cdb2846c229cd905f174b512f3c18f90
sergiocast1 <sergio.castrillo@train.ia.center>
2022-07-23 16:50:36 -0500

    7 Try

--- app.R
+++ app.R
@@ -29,6 +29,25 @@ ui <- fluidPage(
 #                          y = bike[, c('end_lng', 'end_lat')],
 #                          fun = distHaversine))
 
+#mapview(bike, xcol = "start_lat", ycol = "start_lng", crs = 4269, grid = FALSE)
+
+#ggplot(bike, aes(x=as.factor(rideable_type), fill=as.factor(rideable_type) )) + 
+#  geom_bar( ) +
+#  scale_fill_hue(c = 40) +
+#  theme(legend.position="none")    
+
+#ggplot(bike, aes(x=as.factor(start_station_name), fill=as.factor(start_station_name) )) + 
+#  geom_bar( ) +
+#  scale_fill_hue(c = 40) +
+#  theme(legend.position="none")  
+
+#ggplot(bike, aes(x=as.factor(end_station_name), fill=as.factor(end_station_name) )) + 
+#  geom_bar( ) +
+#  scale_fill_hue(c = 40) +
+#  theme(legend.position="none")
+
+#head(bike)
+
 
 # Define server logic required to draw a scatterplot
 server <- function(input, output) {
